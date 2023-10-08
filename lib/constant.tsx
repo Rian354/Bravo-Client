8dd09132236e2eb9c9a439dd2de2243ecf0d36b5
rian354 <rianrian290@icloud.com>
2023-10-08 17:43:00 -0400

    Rian - Fixed API Url

--- lib/constant.tsx
+++ lib/constant.tsx
@@ -1,5 +1,4 @@
-export const BRAVO_SERVER_URL = "http://localhost:8001/v1/";
-//"https://bravo-nasa-server.azurewebsites.net/v1/";
+export const BRAVO_SERVER_URL="https://bravo-nasa-server.azurewebsites.net/v1/";
 
 export const BRAVO_PROJECT_ALL = BRAVO_SERVER_URL + "project";
 export const BRAVO_USER_ALL = BRAVO_SERVER_URL + "user";
